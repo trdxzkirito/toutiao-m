bcf5713833d9e8eea5893ebe3c44aeaecad8c5d3
trdxzkirito <619284842@qq.com>
2021-06-06 22:28:00 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
     <h1>凉席死在了凉席上</h1>
+    <div>
+      <i class="iconfont icon-wode"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <router-view />
   </div>
 </template>
